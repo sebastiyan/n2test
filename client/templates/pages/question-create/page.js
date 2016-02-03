073d933bfa3dd2086a6b91389a8df46233f9587b
Tanya Sebastiyan <tan9seb@gmail.com>
2016-02-03 22:55:56 +0200

    added validation to the form of the Question-create-page

--- client/templates/pages/question-create/page.js
+++ client/templates/pages/question-create/page.js
@@ -1,19 +1,19 @@
 Template.questionCreate.events({
+	"blur textarea": function(e){
+		$(e.target).val($(e.target).val().trim())
+	},
+
 	"submit form": function(e){
 		var $form = $(e.target);
 
-		var name = $form.find(".question-name").val();
-
-		var answers = [
-			$form.find(".answer-1").val(),
-			$form.find(".answer-2").val(),
-			$form.find(".answer-3").val(),
-			$form.find(".answer-4").val()
-		];
-
 		Questions.insert({
-			"name": name,
-			"answers": answers
+			"name": $form.find(".question-name").val().trim(),
+			"answers": [
+			$form.find(".answer-1").val().trim(),
+			$form.find(".answer-2").val().trim(),
+			$form.find(".answer-3").val().trim(),
+			$form.find(".answer-4").val().trim()
+		]
 		})
 		e.preventDefault();
 		return false;
