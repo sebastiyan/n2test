8cb19f6f3bc3db795eb6802cea2cbe271ec22257
Tanya Sebastiyan <tan9seb@gmail.com>
2016-03-06 22:43:07 +0200

    Created pass-test page

--- client/templates/pages/test-list/page.js
+++ client/templates/pages/test-list/page.js
@@ -1,4 +1,23 @@
 Template.testList.events({})
 Template.testList.helpers({
-    testList: Tests.find()
+    settings: function () {
+        return {
+            collection: Tests.find(),
+            rowsPerPage: 10,
+            showFilter: true,
+            fields: [
+                {
+                    key: 'name',
+                    label: 'Test'
+                },
+                {
+                    key: '_id',
+                    label: '',
+                    fn: function (value, object) {
+                        return new Spacebars.SafeString('<a href="/test-pass/'+value+'">Пройти</a>')
+                    }
+                }
+            ]
+        };
+    }
 })
---
+++ client/templates/pages/test-pass/page.js
@@ -0,0 +1,46 @@
+Template.testPass.events({
+    // "blur textarea": function(e){
+    //     $(e.target).val($(e.target).val().trim())
+    // },
+
+    // // get index of radio button right answer
+    // "change input.right-answer": function(e){
+    //     rightAnswerChecked = $(e.target).parent().parent().index();
+    // },
+
+    // "submit form.question-create": function(e){
+    //     var $form = $(e.target);
+
+    //     // save question with answers to the database
+    //     var questionId = Questions.insert({
+    //         "name": $form.find(".question-name").val().trim(),
+    //         "answers": [
+    //         $form.find(".answer-1").val().trim(),
+    //         $form.find(".answer-2").val().trim(),
+    //         $form.find(".answer-3").val().trim(),
+    //         $form.find(".answer-4").val().trim()
+    //         ],
+    //         "testId": this._id
+    //     });
+
+    //     // save right answer to the database
+    //     RightAnswers.insert({
+    //         "index": rightAnswerChecked,
+    //         "questionId": questionId
+    //     });
+
+    //     document.location.reload(true);
+
+    //     e.preventDefault();
+    //     return false;
+    // },
+
+    // "click .end-creation":function(e){
+    //     Router.go("/");
+    // }
+})
+Template.testPass.helpers({
+    questions: function(){
+        return Questions.find({"testId" : this._id})
+    }
+})
