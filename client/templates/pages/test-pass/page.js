a8deae9c49058805c4ab2f1a0b7f2b455e62e8ff
Tanya Sebastiyan <tan9seb@gmail.com>
2016-04-12 19:45:01 +0300

    pass test page

--- client/templates/pages/test-pass/page.js
+++ client/templates/pages/test-pass/page.js
@@ -1,43 +1,21 @@
+Meteor.currentUserId = "u9SusLuhBak7GTKw9";
+
 Template.testPass.events({
-    // "blur textarea": function(e){
-    //     $(e.target).val($(e.target).val().trim())
-    // },
+    "submit .form_pass_test": function(e){
+        var $form = $(e.target);
 
-    // // get index of radio button right answer
-    // "change input.right-answer": function(e){
-    //     rightAnswerChecked = $(e.target).parent().parent().index();
-    // },
+        // save user answers into db
+        $(".question").each( function(){
+            UserAnswers.insert({
+                "userId": Meteor.currentUserId,
+                "questionId": $(this).data("question"),
+                "index": $(this).find("input:checked").parents(".radio").index()
+            });
+        });
 
-    // "submit form.question-create": function(e){
-    //     var $form = $(e.target);
-
-    //     // save question with answers to the database
-    //     var questionId = Questions.insert({
-    //         "name": $form.find(".question-name").val().trim(),
-    //         "answers": [
-    //         $form.find(".answer-1").val().trim(),
-    //         $form.find(".answer-2").val().trim(),
-    //         $form.find(".answer-3").val().trim(),
-    //         $form.find(".answer-4").val().trim()
-    //         ],
-    //         "testId": this._id
-    //     });
-
-    //     // save right answer to the database
-    //     RightAnswers.insert({
-    //         "index": rightAnswerChecked,
-    //         "questionId": questionId
-    //     });
-
-    //     document.location.reload(true);
-
-    //     e.preventDefault();
-    //     return false;
-    // },
-
-    // "click .end-creation":function(e){
-    //     Router.go("/");
-    // }
+        e.preventDefault();
+        return false;
+    }
 })
 Template.testPass.helpers({
     questions: function(){
