848f4d6989534fe047a06f2026172a4eaf17c94a
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2016-04-14 14:37:40 +0300

    Test result page

--- client/templates/pages/test-result/page.js
+++ client/templates/pages/test-result/page.js
@@ -2,13 +2,23 @@ Meteor.currentUserId = "u9SusLuhBak7GTKw9";
 
 Template.testResult.events({
 
-})
+});
 
-Template.testResult.helpers({})
+Template.testResult.helpers({
+  result: function() {
+    var questions = Questions.find({"testId": Template.currentData()._id});
+    var result = 0;
+
+    questions.forEach(function(question) {
+        var rightAnswer = RightAnswers.findOne({"questionId": question._id});
+        var userAnswer = UserAnswers.findOne({"questionId": question._id});
+
+        if(rightAnswer.index === userAnswer.index)
+          result += 1;
+    });
+    return result;
+  }
+});
 
 Template.testResult.onRendered(function(){
-    console.log(Template.currentData());
-    var questions = Questions.find({"_id": this._id}).fetch();
-    console.log(questions);
-})
-
+});
