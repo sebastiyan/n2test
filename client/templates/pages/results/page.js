424eb96764221ee3b54dcb4c8b6f789f65b73504
Tanya Sebastiyan <tan9seb@gmail.com>
2016-05-26 20:31:52 +0300

    results list on results page

--- client/templates/pages/results/page.js
+++ client/templates/pages/results/page.js
@@ -1,2 +1,22 @@
 Template.results.events({})
-Template.results.helpers({})
+Template.results.helpers({
+    resultsSettings: function () {
+        return {
+            id: "results",
+            collection: Results.find({"testId": Template.currentData()._id}),
+            showNavigationRowsPerPage: false,
+            showNavigation: 'auto',
+            showFilter: false,
+            fields: [
+                {
+                    key: "userId",
+                    label: "Студент"
+                },
+                {
+                    key: "result",
+                    label: "Результат"
+                }
+            ]
+        };
+    }
+})
