328020b4694f7ef77049d03c0f92d5db4b7dcce6
Tanya Sebastiyan <tan9seb@gmail.com>
2016-06-01 00:25:36 +0300

    test list for student

--- client/templates/pages/test-list/page.js
+++ client/templates/pages/test-list/page.js
@@ -5,16 +5,34 @@ Template.testList.helpers({
             collection: Tests.find(),
             rowsPerPage: 10,
             showFilter: true,
+            showNavigation: 'auto',
             fields: [
                 {
                     key: 'name',
-                    label: 'Test'
+                    label: 'Назва'
+                },
+                {
+                    key: 'subjectId',
+                    label: 'Предмет',
+                    fn: function (value, object) {
+                        // find name of subject by subjectId in Subjects collection
+                        return Subjects.findOne({ _id: value }).name;
+                    }
+                },
+                {
+                    key: 'creatorId',
+                    label: 'Викладач',
+                    fn: function (value, object) {
+                        // get lectors name and surname that created test
+                        return Meteor.users.findOne({ _id: value }).profile.name + ' ' + Meteor.users.findOne({ _id: value }).profile.surname;
+                        // + Meteor.users.findOne({ _id: value }).surname
+                    }
                 },
                 {
                     key: '_id',
                     label: '',
                     fn: function (value, object) {
-                        return new Spacebars.SafeString('<a href="/test-pass/'+value+'">Пройти</a>')
+                        return new Spacebars.SafeString('<a class="waves-effect waves-light btn" href="/test-pass/'+value+'">Пройти</a>')
                     }
                 }
             ]
